af8923c78e728dacc74af6c00b435ee550dac62c
David Gunn <12180863+rmit-s3429009-david-gunn@users.noreply.github.com>
2017-08-09 17:51:26 +1000

    Update aboutus.blade.php

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,6 +1,19 @@
 <html>
   <header>
+    <title>
+    </title>
   </header>
   <body>
+    <h2>Who are we?</h2>
+    <p>We are a team of Universiry students in our final year who saw a need, and went straight into action!</p>
+    <h2>Why are we here?</h2>
+    <p>The Car Share was an innovation that came out of the need for a better method of sharing transportation in today's society.</p>
+    <p>With increasing numbers of road users, and with each family having multiple cars, the roads are becoming increasingly busy. Due to this, the team at The Car Share decided it was time for a change.</p>
+    <p>Our team saw that so many cars were being driven from A to B, left in an expensive parking spot for the day, and then driven back from B to A. There are so many cars each day sitting for hours doing absolutely nothing, but costing money by occupying space. So why not put those cars to good use, when a driver uses a car to drive in the morning to get to work, it can then be driven by others throughout the day, and when the first driver is ready to go back home, they can simply find a car using our service, and make their way to home.</p>
+    <p>Stop paying for car registration, insurance, parking, permits, etc. and join us at The Car Share.</p>
+    <h2>Our Vision for the now</h2>
+    <p>To see the amount of vehicles on the road and in parking lots reduced significantly, with the cars that are around being utilised to their proper potential</p>
+    <h2>Our Vision for the Future</h2>
+    <p>We want to see Driveless Cars in action, increasing safety for passengers and pedestrians. We want these cars to be bookable by our service, and for them to drive to you as opposed to you finding them in a lot. This is the future. This is The Car Share.</p>
   </body>
 </html>
