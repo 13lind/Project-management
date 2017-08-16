ad8c97e14a184e94959c850e2e67f1b8157ee914
rmit-s3437103-lovepreet-kaur <s3437103@student.rmit.edu.au>
2017-08-16 17:33:25 +1000

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,23 @@
-@extends('layout')
+<div class=“Profile“>
 
+ 
+ 
+ <div class="container">
 
-@section('content')
+<h1>Name:</h1>
 
+<h1>Email:</h1>
 
-<!-- code here -->
+<h1>Mobile:</h1>
 
+<h1>Car chosen: </h1>
 
-@endsection
+<h1>Dates</h1>
+ 
+
+    
+<p><button>Contact</button></p>
+
+  </div>
+
+</div>
