b3a141ce15a631ab3e48a29a78634b87a4790c2d
rmit-s3437103-lovepreet-kaur <s3437103@student.rmit.edu.au>
2017-08-21 20:34:09 +1000

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,26 +13,68 @@
 	
 
 
-<div class=“Profile“>
+<div class="container">
+<form>
 
+ <div style="background-color:lightblue;color:white;padding:2px;">
+
+<h2>Personal Details</h2>
+
+</div>
+
+
+<br><br>
+
+First Name:
+<input type=“text” name=“firstname”>
+
+
+<br>
+<br>
+
+Last Name:
+<input type=“text” name=“lastname”>
+
+<br>
+<br>
+
+Email:
+<input type=“text” name=“email”>
+
+<br>
+<br>
+
+Mobile Phone:
+<input type=“text” name=“mobile” value=0412648263>
+
+<br>
+<br>
+————————————————————————
+<br>
+<br>
+
+<div style="background-color:lightblue;color:white;padding:2px;">
+
+<h2>Car Details</h2>
+
+</div>
+
+<br><br>
+
+Type of Car chosen: 
+<input type=“text” name=“carname”><br>
+
+<br>
+
+<th>Dates being borrowed:</th>
+<td><input type=“text” name=“dates”></td><br>
  
- 
- <div class="container">
+<input type=“submit” value=“submit”>
 
-<h1>Name:</h1>
-
-<h1>Email:</h1>
-
-<h1>Mobile:</h1>
-
-<h1>Car chosen: </h1>
-
-<h1>Dates being borrowed:</h1>
- 
-
+</form>
     
 
-  </div>
+  
 
 </div>
 </body>
