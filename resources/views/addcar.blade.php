fbe0457f573b509775c497f2da70c7f6452e83dc
Kaushik Prayaga <s3436435@student.rmit.edu.au>
2017-09-06 15:44:35 +1000

    Update addcar.blade.php

--- resources/views/addcar.blade.php
+++ resources/views/addcar.blade.php
@@ -1,17 +1,22 @@
 @extends('layout')
 @section('content')
 
-<h2>Add Car</h2>
+<link rel="stylesheet" href="addcar.css">
+<body>
+<div class="video-background">
+    <div class="video-foreground">
+		<iframe width="854" height="480" src="https://www.youtube.com/embed/5_VR31lrtl8?&amp;rel=0&amp;controls=0&amp;showinfo=0;loop=1&amp;autoplay=1&amp" frameborder="0" allowfullscreen></iframe>
+    </div>
+</div>
 
+<div class="container">
 <form method="POST" action="">
-		   
-
-
+	<h2>Add Car</h2>
 	<div>
 		<label>Car Make</label>
 
 		<div>
-			<input id="make" type="text" name="make" placeholder="Toyota" 
+			<input id="make" type="text" name="make" placeholder="Ford" 
 			 required autofocus>
 		</div>
 	</div>
@@ -20,7 +25,7 @@
 		<label>Car Model</label>
 
 		<div>
-			<input id="model" type="text" name="model" placeholder="Corolla" 
+			<input id="model" type="text" name="model" placeholder="Falcon" 
 			 required autofocus>
 		</div>
 	</div>
@@ -57,7 +62,7 @@
 		<label>Street Address</label>
 
 		<div>
-			<input id="street_address" type="text" name="street_address" placeholder="124 La Trobe Street" 
+			<input id="street_address" type="text" name="street_address" placeholder="123 Smith Street" 
 			 required autofocus>
 		</div>
 	</div>					
@@ -66,7 +71,7 @@
 		<label>Suburb</label>
 
 		<div>
-			<input id="surburb" type="text" name="surburb" placeholder="Melbourne" 
+			<input id="suburb" type="text" name="suburb" placeholder="Fitzroy" 
 			 required autofocus>
 		</div>
 	</div>		
@@ -84,7 +89,7 @@
 		<label>Postcode</label>
 
 		<div>
-			<input id="postcode" type="text" name="postcode" placeholder="3000" 
+			<input id="postcode" type="text" name="postcode" placeholder="3065" 
 			 required autofocus>
 		</div>
 	</div>
@@ -92,5 +97,7 @@
 	<button>Submit</button>
 
 </form>
+</div>
+</body>
 
 @endsection
