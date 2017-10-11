d82ac9cea6eb65742e40380d91cdc317979be95a
rmit-s3435317-adamradovic <30649265+rmit-s3435317-adamradovic@users.noreply.github.com>
2017-10-11 13:41:26 +1100

    Update AppServiceProvider.dusk.php

--- app/Providers/AppServiceProvider.dusk.php
+++ app/Providers/AppServiceProvider.dusk.php
@@ -1,32 +1,32 @@
 <?php
 
-namespace App\Providers;
+// namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use Laravel\Dusk\DuskServiceProvider;
+// use Illuminate\Support\ServiceProvider;
+// use Laravel\Dusk\DuskServiceProvider;
 
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+// class AppServiceProvider extends ServiceProvider
+// {
+//     /**
+//      * Bootstrap any application services.
+//      *
+//      * @return void
+//      */
+//     public function boot()
+//     {
+//         //
+//     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        if ($this->app->environment('local', 'testing')) {
-            $this->app->register(DuskServiceProvider::class);
-        }
-    }
-}
+//     /**
+//      * Register any application services.
+//      *
+//      * @return void
+//      */
+//     public function register()
+//     {
+//         if ($this->app->environment('local', 'testing')) {
+//             $this->app->register(DuskServiceProvider::class);
+//         }
+//     }
+// }
